17e7f77f8c70f343b85406c7f8a7a252221d9dfd
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2023-10-20 13:32:36 +0300

    Update popup.js
    added pause/delete and user input

--- popup.js
+++ popup.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('startTimer').addEventListener('click', function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.runtime.sendMessage({ startTimer: true });
-            window.close();
-        });
+    var startTimerBtn = document.getElementById('startTimer');
+    var pauseTimerBtn = document.getElementById('pauseTimer');
+    var deleteTimerBtn = document.getElementById('deleteTimer');
+    var timerInput = document.getElementById('timerInput');
+
+    startTimerBtn.addEventListener('click', function () {
+        var timerMinutes = timerInput.value;
+        var timerDuration = timerMinutes * 60;
+        chrome.runtime.sendMessage({ startTimer: true, duration: timerDuration });
+        window.close();
+    });
+
+    pauseTimerBtn.addEventListener('click', function () {
+        chrome.runtime.sendMessage({ pauseTimer: true });
+    });
+
+    deleteTimerBtn.addEventListener('click', function () {
+        chrome.runtime.sendMessage({ deleteTimer: true });
     });
 });
